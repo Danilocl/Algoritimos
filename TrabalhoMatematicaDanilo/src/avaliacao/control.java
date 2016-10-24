9c7b7b7b13b2562ce5de94d52749ed51adbd4ac8
tempor <tempor@L306-03.senacrjedu.local>
2016-10-24 21:36:22 -0200

    mudança para booleano e começo da criação da tabela.

--- TrabalhoMatematicaDanilo/src/avaliacao/control.java
+++ TrabalhoMatematicaDanilo/src/avaliacao/control.java
@@ -1,44 +1,44 @@
 package avaliacao;
 
 public class control {
-	public static String control2(String p, String q, String r) {
+	public static boolean control2 (boolean p,boolean q,boolean r) {
 
-	String[] tabela = new String[7];
+	boolean[] tabela = new boolean[7];
 
-		if ((q == "true") && (r == "true")) {
+		if ((q == true) && (r == true)) {
 			// Primeiro
-			tabela[1] = "true";
+			tabela[1] = true;
 		} else {
-			tabela[1] = "false";
+			tabela[1] = false;
 		}
 		// P Disjunção q e r Segundo Parêntese
-		if ((p == "false") && (tabela[1] == "false")) {
+		if ((p == false) && (tabela[1] == false)) {
 			// P Disjunção q e r
-			tabela[2] = "false";
+			tabela[2] = false;
 		} else {
-			tabela[2] = "true";
+			tabela[2] = true;
 		}
-		if ((p == "false") && (q == "false")) {
-			tabela[3] = "false";
+		if ((p == false) && (q == false)) {
+			tabela[3] = false;
 		} else {
-			tabela[3] = "true";
+			tabela[3] = true;
 		}
-		if ((p == "false") && (r == "false")) {
-			tabela[4] = "false";
+		if ((p == false) && (r == false)) {
+			tabela[4] = false;
 		} else {
-			tabela[4] = "true";
+			tabela[4] = true;
 		}
-		if ((tabela[3] == "true") && (tabela[4] == "true")) {
-			tabela[5] = "true";
+		if ((tabela[3] == true) && (tabela[4] == true)) {
+			tabela[5] = true;
 		} else {
-			tabela[5] = "false";
+			tabela[5] = false;
 		}
-		if ((tabela[2] == "true") && (tabela[5] == "true")) {
-			tabela[6] = "true";
-		} else if ((tabela[2] == "false") && (tabela[5] == "false")) {
-			tabela[6] = "true";
+		if ((tabela[2] == true) && (tabela[5] == true)) {
+			tabela[6] = true;
+		} else if ((tabela[2] == false) && (tabela[5] == false)) {
+			tabela[6] = true;
 		} else {
-			tabela[6] = "false";
+			tabela[6] = false;
 		}
 
 		return tabela[6];
@@ -46,33 +46,22 @@ public class control {
 	}
 
 	public static void tabela2(boolean p, boolean q, boolean r) {
-		if (r == true) {
-			System.out.println("V");
-		} else {
-			System.out.println("F");
-		}
-		if (q == true) {
-			System.out.println("V");
-		} else {
-			System.out.println("F");
-		}
-		if (p == true) {
-			System.out.println("V");
-		} else {
-			System.out.println("F");
-		}
+		System.out.println("|P|Q|R|");
+	    System.out.printf(""+p,q,r);
+	
+	}
+	
+
+//	public static String erro (String x) {
+//		String y;
+//		if (x=="true") {
+//		y = "true";
+//		 } else if (x=="false") {
+//			 y ="false";
+//		} else {
+//			System.out.println("O valor digitado é invalido ");
+//			System.out.println("Favor digite false para (Falso) e true para (verdadeiro) ");
+//		}
+//		return x;
 	}
 
-	public static String erro (String x) {
-		String y;
-		if (x=="true") {
-		y = "true";
-		 } else if (x=="false") {
-			 y ="false";
-		} else {
-			System.out.println("O valor digitado é invalido ");
-			System.out.println("Favor digite false para (Falso) e true para (verdadeiro) ");
-		}
-		return x;
-	}
-}
--- TrabalhoMatematicaDanilo/src/avaliacao/main2.java
+++ TrabalhoMatematicaDanilo/src/avaliacao/main2.java
@@ -6,28 +6,32 @@ import java.util.Scanner;
 public class main2 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String p, q, r;
+		boolean p, q, r;
 		System.out.println("Digite 'true' para verdadeiro e 'false' para falso'");
 		System.out.println("                                                   ");
 		System.out.println("                                                   ");
 		System.out.println("Pv(q^r) <==> (p V q0 ^(pVr)");
-
+		System.out.println("                                                   ");
+		
 		System.out.println("Digite o valor de r");
-		r = in.nextLine();
+		r = in.nextBoolean();
 		
 		
 		System.out.println("Digite o valor de q");
-		q = in.nextLine();
+		q = in.nextBoolean();
 		
 
 		System.out.println("Digite o valor de P");
-		p = in.nextLine();
+		p = in.nextBoolean();
 		
 		
+		System.out.println("\n");
 		
-		String resultado = control.control2(p, q, r);
-
-		System.out.println(" O resultado é " + resultado);
+		boolean resultado = control.control2 (p, q, r);
+        
+		control.tabela2(p, q, r);
+		
+		System.out.println("\n O resultado é " + resultado);
 		in.close();
 	
 	}
